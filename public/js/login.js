a0cc9d93c95a2b4f932b104ba75e78cd75660799
이성현 <123540125+bluegown@users.noreply.github.com>
2024-05-14 17:56:43 +0900

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -2,64 +2,34 @@
 // 2번조건도 완료
 // 3번도 구현 완료!!
 
-function lengthid(value) {
-  if (value.length >= 4) {
-    return true;
-  } else {
-    return false;
-  }
-}
+const lengthid = (value) => (value.length >=4);
+const Pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-function validateEmail(email) {
-  const Pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return Pattern.test(email);
-} 
+const validateEmail = (email) => Pattern.test(email); // 유효한 경우
+
 const joinButton = document.getElementById('join'); 
 joinButton.addEventListener('click',() =>{
   window.location.href = "join";
 });
-function checkPassword(password) {
+const checkPassword = (password) => {
   const hasUpperCase = /[A-Z]/.test(password);
   const hasLowerCase = /[a-z]/.test(password);
   const hasNumber = /[0-9]/.test(password);
   const hasSpecialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(
     password,
   );
-  if (password.length < 8 || password.length > 20) {
-    return false;
-  }
-  if (!(hasUpperCase && hasLowerCase && hasNumber)) {
-    return false;
-  } // 셋중 하나라도 만족 못하면 return false
-  if (!hasSpecialChar) {
-    return false;
-  } // 특수문자 없는 경우
+  return password.length >= 8 && password.length <= 20 &&
+         hasUpperCase && hasLowerCase && hasNumber && hasSpecialChar;
+};
 
-  return true;
-}
+const checkInformation = (users,userid,password) => users.some(users => users.userid == userid && password == users.password);
 
-function checkInformation(users,userid,password){
-  for (let i = 0; i< element.length; i++){
-    if (userid == users[i].userid && password == users[i].password)
-    return true;
-  }
-  return false;
-}
 
-function checkLogin(users,userid,password){
-  for (let i = 0; i<users.length; i++){
-  if(userid == users[i].userid){
-    if (password == users[i].password){
-      return true; // id, pw 모두 일치하는 경우
-    }
-    else{
-      return false;
-    } // 아이디는 안에 존재하지만 비밀번호는 없는 경우
-  }
-
-  }
+const checkLogin = (users,userid,password) => {
   
-}
+  return users.some(users => users.password == password);
+  
+};
 // ../면 /Users/sunghyun/Desktop/인데..
 // ./는 현재 디렉토리 ../는 상위 디렉토리
 fetch("../data.json")
@@ -78,8 +48,8 @@ fetch("../data.json")
     t.addEventListener("submit", function (event) {
       event.preventDefault(); // 이걸 통해 디폴트로 일어나는 것들을 방지하고
 
-      var userid = document.getElementById("userid").value; // 입력시에 유저아이디
-      var password = document.getElementById("password").value; // 입력시에 비밀번호
+      let userid = document.getElementById("userid").value; // 입력시에 유저아이디
+      let password = document.getElementById("password").value; // 입력시에 비밀번호
       if (userid == "") {
         button.innerText = '이메일을 입력해주세요.'
         return false;
@@ -95,7 +65,7 @@ fetch("../data.json")
       } else if (!checkPassword(password)) {
         button.innerText =
           "비밀번호는 8자 이상, 20자 이하이며, 대문자, 소문자, 특수문자를 각각 최소 1개 포함해야 합니다.";
-        return false;
+        return false
       } else if (!checkLogin(users,userid,password)) {
         button.innerText ="비밀번호가 다릅니다."; // 바보냐 json에서 배열로 선언해두고 인덱스를 안하니 당연히 이상한값이 들어가지
         // false로 반환되었다면 아이디는 리스트 안에 있지만 비밀번호가 다른 경우 
